eb369826d6e971533dca8e47ecb1f021ad433997
sanjayreddy-nath <sanjayreddy0305@gmail.com>
2025-01-24 13:27:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+// import { get } from "../signup/signup";
+// let signup =document.getElementById("signup")
+
+// signup.addEventListener("click",(e)=>{
+// e.preventDefault
+// console.log(get)
+// })
+
+
+let get=document.getElementById("exampleModal");
+// Load modal content dynamically
+document.addEventListener("DOMContentLoaded", function () {
+    // Load the Signup Modal
+    fetch("../signup/signup.html")
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById("modalContainer").insertAdjacentHTML("beforeend", html);
+        });
+
+    // Load the Login Modal
+    fetch("../login/login.html")
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById("modalContainer").insertAdjacentHTML("beforeend", html);
+        });
+});
